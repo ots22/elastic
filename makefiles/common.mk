087fe012fcc9bb345ec308e06281fca5b0ec5fed
Oliver Strickson <ots22@cam.ac.uk>
2017-04-12 13:49:38 +0100

    Scope for building more than one program (existing main.F90 is now programs/elastic2d.F90)

--- makefiles/common.mk
+++ makefiles/common.mk
@@ -1,9 +1,15 @@
 SRC_DIR = ../../src
 SRC = $(wildcard $(SRC_DIR)/*.f90 $(SRC_DIR)/*.F90)
+PROG_SRC = $(wildcard $(SRC_DIR)/programs/*.f90 $(SRC_DIR)/programs/*.F90)
 TEST_SRC = $(wildcard $(SRC_DIR)/test/*.f90 $(SRC_DIR)/test/*.F90)
 
 OBJ = $(notdir $(SRC:.f90=.o))
 OBJ := $(notdir $(OBJ:.F90=.o))
+
+PROG_OBJ = $(notdir $(PROG_SRC:.f90=.o))
+PROG_OBJ := $(notdir $(PROG_OBJ:.F90=.o))
+
+PROGS = $(basename $(PROG_OBJ))
 
 TEST_OBJ = $(notdir $(TEST_SRC:.f90=.o))
 TEST_OBJ := $(notdir $(TEST_SRC:.F90=.o))
@@ -14,13 +20,16 @@ FCFLAGS += -L../../lib
 FCFLAGS += -I../../lib
 
 
-$(SRC_DIR)/depend: $(SRC) $(TEST_SRC)
+$(SRC_DIR)/depend: $(SRC) $(PROG_SRC) $(TEST_SRC)
 	makedepf90 -W -m"%m.mod" -b"." $^ > $@
 
 -include $(SRC_DIR)/depend
 
-elastic2d: $(SRC_DIR)/depend $(ALL_OBJ)
-	$(FC) $(FCFLAGS) $(ALL_OBJ) -o elastic2d $(LDFLAGS)
+#elastic2d: $(SRC_DIR)/depend $(OBJ) elastic2d.o
+#	$(FC) $(FCFLAGS) $(OBJ) elastic2d.o -o elastic2d $(LDFLAGS)
+
+$(PROGS) : % : $(SRC_DIR)/depend $(OBJ) %.o
+	$(FC) $(FCFLAGS) $(OBJ) $@.o -o $@ $(LDFLAGS)
 
 $(TESTS) : test/% : $(SRC_DIR)/depend $(OBJ) %.o
 	$(FC) $(FCFLAGS) $(OBJ) $(notdir $@).o -o $@ $(LDFLAGS)
@@ -32,3 +41,4 @@ $(TESTS) : test/% : $(SRC_DIR)/depend $(OBJ) %.o
 
 %.o:
 	$(FC) $(FCFLAGS) $(filter %.f90 %.F90, $^) -c -o $@
+
