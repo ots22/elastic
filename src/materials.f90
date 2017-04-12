65569100023ad3d418da8a1d48e203490463017a
Oliver Strickson <ots22@cam.ac.uk>
2017-04-12 12:19:03 +0100

    Add example Mooney-Rivlin material

--- src/materials.f90
+++ src/materials.f90
@@ -3,9 +3,10 @@
 
 module m_materials
   use m_eos_romenski
+  use m_eos_mooney_rivlin
 
   private
-  public copper_romenski
+  public copper_romenski, example_material_mr
 
   real, parameter  :: c0=4.6, b0=2.1
 ! alpha, beta and gamma are not exact integers to avoid a bug in glibc
@@ -18,4 +19,13 @@ module m_materials
                     gamma = 2.00001, &
                     cv    = 3.9E-4, &
                     T0    = 300.0)
+
+  type(eos_mooney_rivlin), parameter :: example_material_mr = &
+       eos_mooney_rivlin(rho0 = 1.0, &
+                         mu_0 = 0.6, &
+                         lambda_0 = 0.6, &
+                         mu_s = 0.01, &
+                         lambda_s = 0.01, &
+                         theta_0 = 0.1, &
+                         theta_1 = 10.0)
 end module m_materials
